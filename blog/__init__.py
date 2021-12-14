b73bbb185ab1183c38aaa907835b8cd561b2e4a1
Matthew Robinson <robinsonm18@cardiff.ac.uk>
2021-12-14 11:26:49 +0000

    models and routes added for database + requirements updated for SQL

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,6 +1,16 @@
-from flask import Flask 
+from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '86a13d29b940a84c8c8a067871c226da70397817097eaf3b' 
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'blog.db')
+
+db = SQLAlchemy(app)
 
 from blog import routes
