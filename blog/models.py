6f5e30caeb9fcba9d1ae1c75cc42dcdf50521a09
Matthew Robinson <robinsonm18@cardiff.ac.uk>
2021-12-21 20:52:15 +0000

    Removed Database

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(db.Model):
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(15), unique=True, nullable=False)
-  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(120), nullable=False)
   post = db.relationship('Post', backref='user', lazy=True)
 
   def __repr__(self):
