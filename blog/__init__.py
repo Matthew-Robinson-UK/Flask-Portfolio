4dc7bee60123547d6fede88bb4069dfd740c39f8
Matthew Robinson <robinsonm18@cardiff.ac.uk>
2021-12-30 21:47:50 +0000

    defined static folder in app

--- blog/__init__.py
+++ blog/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = '86a13d29b940a84c8c8a067871c226da70397817097eaf3b' 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
