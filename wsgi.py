936521fb7eed8e224f4b2ea66feb4c3c03629c56
Matthew Robinson <robinsonm18@cardiff.ac.uk>
2022-01-19 09:04:12 +0000

    wsgi update

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-# from blog import app as application
+from blog import app as application
 
-from blog import app
+# from blog import app
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+# if __name__ == '__main__':
+#     app.run(debug=True) 
